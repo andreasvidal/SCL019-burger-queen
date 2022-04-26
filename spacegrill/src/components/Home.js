866dbc4a7ce16f230a31fce8160e4dd627764e25
andreasvidal <andrea.s.vidal00@gmail.com>
2022-04-26 10:02:49 -0400

    OH con Marcia, se intala manualmente react router

---
+++ spacegrill/src/components/Home.js
@@ -0,0 +1,16 @@
+import logo from "../resources/logo.png"
+
+export const Home =()=>{
+  <main>
+  <div className="container-img"> <img className="img-logo" alt="logo priencipal">{logo}</img>  </div>
+    <div className="container">
+      <h1 className="title-home">SPACEGRILL</h1>
+      <p>Burger</p>
+    </div>
+    <div className="container-links">
+    <link rel="stylesheet" href="../src/components/kitchen/Kitchen.js" />
+      <link rel="stylesheet" href="" />
+    </div>
+  </main>
+}
+
