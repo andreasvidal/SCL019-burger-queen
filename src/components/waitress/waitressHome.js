ef649fa1a6788745ea40c2ca0d27898b0a107259
andreasvidal <andrea.s.vidal00@gmail.com>
2022-04-28 08:09:21 -0400

    se elimina carpeta spacegrill, sigue sin mostrar info

---
+++ src/components/waitress/waitressHome.js
@@ -0,0 +1,25 @@
+
+export const waitressHome = () =>{
+  <main>
+  /*header*/
+    <div>
+      <h1>AGREGAR PEDIDOS</h1>
+      <label for="order-time">Order Time</label>
+      <input id="order-time" type="datetime-local" name="order-time" value="2017-06-01T08:30"></input>
+    </div>
+    <form action=""  method="post" className="form-container">
+      <div className="form-section">
+        <label for="waitress">MESERO</label>
+        <input type="text" name="name" id="name" required />
+      </div>
+      <div className="form-section">
+        <label for="waitress">N° MESA</label>
+        <input type="text" name="name" id="name" required />
+      </div>
+    </form>
+    /*productos*/
+    <button></button>
+  </main>
+}
+
+
